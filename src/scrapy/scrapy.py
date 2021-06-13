6ac9a8f601f6765c94d94f144da1b458a4c48731
m-star18 <31807@toyota.kosen-ac.jp>
2021-06-13 23:02:02 +0900

    Add comment

--- src/scrapy/scrapy.py
+++ src/scrapy/scrapy.py
@@ -13,6 +13,34 @@ class SuumoScrapyJob:
         """
         Attributes
         ----------
+        self.name: list of str
+            マンション名.
+        self.address: list of str
+            住所.
+        self.locations0: list of str
+            立地1つ目（最寄駅/徒歩~分.
+        self.locations1: list of str
+            立地2つ目（最寄駅/徒歩~分）.
+        self.locations2: list of str
+            立地3つ目（最寄駅/徒歩~分）.
+        self.age: list of int
+            築年数.
+        self.height: list of int
+            建物高さ.
+        self.floor. list of int
+            階.
+        self.rent: list of int
+            賃料.
+        self.admin: list of int
+            管理費.
+        self.deposit: list of int
+            敷金.
+        self.others: list of int
+            礼金.
+        self.floor_plan: list of str
+            間取り.
+        self.area: list of int
+            専有面積.
         """
         self.result = requests.get(URL)
         self.content = self.result.content
