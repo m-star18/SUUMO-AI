067ede51c241334eaec0db0a63e1222e5bdc6d90
m-star18 <31807@toyota.kosen-ac.jp>
2021-06-14 00:22:54 +0900

    Update name

--- src/scrapy/const.py
+++ src/scrapy/const.py
@@ -1,4 +1,5 @@
 URL = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=050&bs=040&ra=023&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&ek=310004650&ek=310026560&rn=3100'
+NAME = 'toyotashi'
 FLOOR = 0
 RENT = 7
 ADMIN = 8
